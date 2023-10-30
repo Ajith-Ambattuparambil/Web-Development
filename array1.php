8c8d465f9c0dc9b8d6752e52462f6383801f7efe
Ajith Mani Santhosh <146512478+Ajith-Ambattuparambil@users.noreply.github.com>
2023-10-30 11:28:17 +0530

    Add files via upload

---
+++ array1.php
@@ -0,0 +1,9 @@
+<?php
+$arr=array("12","13","15");
+$h=count($arr);
+for($i=0;$i<$h;$i++)
+  echo $arr[$i],"<br>";
+ $s=array("S1 MCA" => "60","S2 MCA" => "30");
+foreach($s as $v => $n)
+  echo $v,"=",$n,"<br>";
+?>
---
+++ array2.php
@@ -0,0 +1,8 @@
+<?php
+$rno =array("1"=>"Aadarsh","2"=>"Aakash","3"=>"Abhinav","4"=>"Abhinadh","5"=>"Abin");
+echo"<br><ol type = 1>";
+foreach($rno as $value=>$name)
+echo "<li>Name: " , $name , " Roll No.: " , $value ,"<hr></li>";
+echo "<ol>";
+?>
+
