9c56fa1f9bd9a98185b99beacc80f7967c4c19a0
Ajith Mani Santhosh <146512478+Ajith-Ambattuparambil@users.noreply.github.com>
2023-11-22 11:43:47 +0530

    Update 4thpgm.php

--- 4thpgm.php
+++ 4thpgm.php
@@ -1,5 +1,9 @@
 <html>
 <body>
+<center>
+<head><h1>Random Number</h1></head>
 <iframe src="4thpgmrand.php" width="260px" height="195px"></iframe>
+<iframe src="4thpgmrand.php" width="260px" height="195px"></iframe>
+</center>
 </body>
 </html>
